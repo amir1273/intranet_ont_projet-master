3e2a06966dbd3ca620d0810051e261b9f2845755
narutomk <khaled.bouflija@gmail.com>
2022-07-07 16:50:46 +0100

    <<<<<<<<

--- src/pages/EmployéPage/AjoutdemandePret.js
+++ src/pages/EmployéPage/AjoutdemandePret.js
@@ -1,40 +1,90 @@
-import React from 'react';
-import Logo from '../../components/Logo';
-import Navigation from '../../components/Navigation';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Logo from "../../components/Logo";
+import Navigation from "../../components/Navigation";
 
 const AjoutdemandePret = () => {
-    return (
-        <div>
-            <Logo />
-            <Navigation />
-            <div className="container">
-                <div className="title">Ajouter une demande de prêt</div>
-                <div className="content">
-                    <form action="#">
-                        <div className="user-details">
-                            <div className="input-box">
-                                <span className="details">Matricule Employé</span>
-                                <input type="text" name="matricule" placeholder="Entrer Le matricule de" disabled />
-                            </div>
-                            <div className="input-box">
-                                <span className="details">Nom et prénom</span>
-                                <input type="text" name='nomComplet' placeholder="Entrer Le nom complet de " disabled />
-                            </div>
+  const [user, setUser] = useState({});
+  const [done, setDone] = useState(false);
+  const [error, setError] = useState("");
+  const [montant, setMontant] = useState();
 
-                            <div className="input-box">
-                                <span className="details">Montant </span>
-                                <input type="double" name='soldeConge' placeholder="Entrer le montant désirer" />
-                            </div>
-                        </div>
-                        <div className="button">
-                            <input type="submit" value="Enregistrement" />
-                        </div>
-                    </form>
-                </div>
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+
+    return () => {};
+  }, []);
+
+  const addPret = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        `http://localhost:8080/pret/add/${localStorage.getItem("username")}`,
+        {
+          montant,
+        },
+        {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+      )
+      .then((r) => {
+        setDone(true);
+        navigate("/GestionPrets");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <div className="container">
+        <div className="title">Ajouter une demande de prêt</div>
+        <div className="content">
+          <form action="#">
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Matricule Employé</span>
+                <input
+                  type="text"
+                  name="matricule"
+                  placeholder="Entrer Le matricule de"
+                  disabled
+                  value={user.matricule}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Nom et prénom</span>
+                <input
+                  type="text"
+                  name="nomComplet"
+                  placeholder="Entrer Le nom complet de "
+                  disabled
+                  value={user.nomComplet}
+                />
+              </div>
+
+              <div className="input-box">
+                <span className="details">Montant </span>
+                <input
+                  type="double"
+                  name="montant"
+                  placeholder="Entrer le montant désirer"
+                  onChange={(e) => setMontant(e.target.value)}
+                  value={montant}
+                />
+              </div>
             </div>
-
+            <div className="button">
+              <input type="button" value="Enregistrement" onClick={addPret} />
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AjoutdemandePret;
--- src/pages/EmployéPage/GestionPrêts.js
+++ src/pages/EmployéPage/GestionPrêts.js
@@ -1,51 +1,109 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Logo from '../../components/Logo';
-import Navigation from '../../components/Navigation';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../components/Logo";
+import Navigation from "../../components/Navigation";
 
 const GestionPrêts = () => {
-    return (
-        <div>
-            <Logo />
-            <Navigation />
-            <h1>Gestion des Prêts</h1>
-            <center>
-                <div className="buttonC w-50 ">
-                    <NavLink to="/AjoutDemandePret" >
-                        <input className="buttonB" type="button" value="Déposer une demande de Prêts" />
-                    </NavLink>
-                </div>
-            </center>
-            <table className="content-tableC">
-                <thead>
-                    <tr>
-                        <th>Rang</th>
-                        <th>Matricule</th>
-                        <th>Nom et Prénom</th>
-                        <th>Montant</th>
-                        <th>Décision</th>
-                        <th>Action</th>
+  const [pret, setPret] = useState([]);
+  const [user, setUser] = useState({});
+  const [refresh, setRefresh] = useState(false);
 
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <NavLink to="/ModifierPret" title="Modifier">
-                                <i class="material-icons">&#xE8B8;</i>
-                            </NavLink>
-                            <a href="/SupprimerEmployes" className="delete" title="Delete"><i class="material-icons">&#xE5C9;</i></a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+    getConges();
+
+    return () => {};
+  }, [refresh]);
+
+  const getConges = () => {
+    axios
+      .get(
+        `http://localhost:8080/pret/empl/${localStorage.getItem("username")}`,
+        {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+      )
+      .then((r) => {
+        setPret(r.data);
+        console.log("sucess", pret);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>Gestion des Prêts</h1>
+      <center>
+        <div className="buttonC w-50 ">
+          <NavLink to="/AjoutDemandePret">
+            <input
+              className="buttonB"
+              type="button"
+              value="Déposer une demande de Prêts"
+            />
+          </NavLink>
         </div>
-    );
+      </center>
+      <table className="content-tableC">
+        <thead>
+          <tr>
+            <th>Rang</th>
+            <th>Matricule</th>
+            <th>Nom et Prénom</th>
+            <th>Montant</th>
+            <th>Décision</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pret?.length > 0 && (
+            <>
+              {pret.map((c, index) => {
+                return (
+                  <tr>
+                    <td>{index}</td>
+                    <td>{user.matricule}</td>
+
+                    <td>{user.nomComplet}</td>
+                    <td>{c.montant} DNT</td>
+
+                    <td>{c.statut}</td>
+
+                    <td>
+                      <NavLink to="/ModifierPret" title="Modifier">
+                        <i class="material-icons">&#xE8B8;</i>
+                      </NavLink>
+                      <span
+                        className="delete"
+                        title="Delete"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          axios
+                            .post("http://localhost:8080/pret/remove/", c, {
+                              Authorization: `Bearer ${localStorage.getItem(
+                                "token"
+                              )}`,
+                            })
+                            .then((r) => {
+                              setRefresh(!refresh);
+                            })
+                            .catch((err) => console.log(err));
+                        }}
+                      >
+                        <i class="material-icons">&#xE5C9;</i>
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default GestionPrêts;
