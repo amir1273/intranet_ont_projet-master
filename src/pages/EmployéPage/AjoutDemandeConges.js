dd72f588fc60e00c86803cb8e6efd3b3e256adc7
narutomk <khaled.bouflija@gmail.com>
2022-07-07 03:56:45 +0100

    <<<<<<<<<<<

--- src/pages/EmployéPage/AjoutDemandeConges.js
+++ src/pages/EmployéPage/AjoutDemandeConges.js
@@ -1,48 +1,111 @@
-import React from 'react';
-import Logo from '../../components/Logo';
-import Navigation from '../../components/Navigation';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Logo from "../../components/Logo";
+import Navigation from "../../components/Navigation";
 
 const AjoutDemandeConges = () => {
-    return (
-        <div>
-            <Logo />
-            <Navigation />
-            <div className="container">
-                <div className="title">Ajouter une demande de congé</div>
-                <div className="content">
-                    <form action="#">
-                        <div className="user-details">
-                            <div className="input-box">
-                                <span className="details">Matricule Employé</span>
-                                <input type="text" name="matricule" placeholder="Entrer Le matricule de" disabled />
-                            </div>
-                            <div className="input-box">
-                                <span className="details">Nom et prénom</span>
-                                <input type="text" name='nomComplet' placeholder="Entrer Le nom complet de " disabled />
-                            </div>
-                            <div className="input-box">
-                                <span className="details">Date de Début</span>
-                                <input type="Date" name='datedebutConge' required />
-                            </div>
-                            <div className="input-box">
-                                <span className="details">Date de la fin</span>
-                                <input type="Date" name='dateFinConge' required />
-                            </div>
+  const [conges, setConges] = useState({});
+  const [user, setUser] = useState({});
+  const [done, setDone] = useState(false);
 
-                            <div className="input-box">
-                                <span className="details">Solde Congés</span>
-                                <input type="double" name='soldeConge' placeholder="Entrer le solde des congés" disabled />
-                            </div>
-                        </div>
-                        <div className="button">
-                            <input type="submit" value="Enregistrement" />
-                        </div>
-                    </form>
-                </div>
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+
+    return () => {};
+  }, []);
+
+  const addConge = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        `http://localhost:8080/conges/add/${localStorage.getItem("username")}`,
+        conges,
+        {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+      )
+      .then((r) => {
+        setConges(r.data);
+        console.log("sucess", conges);
+        setDone(true);
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleAddConge = (e) => {
+    console.log(conges);
+    const value = e.target.value;
+    setConges({
+      ...conges,
+      [e.target.name]: value,
+    });
+  };
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <div className="container">
+        <div className="title">Ajouter une demande de congé</div>
+        <div className="content">
+          <form>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Matricule Employé</span>
+                <input
+                  type="text"
+                  name="matricule"
+                  placeholder="Entrer Le matricule de"
+                  disabled
+                  value={user.matricule}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Nom et prénom</span>
+                <input
+                  type="text"
+                  name="nomComplet"
+                  placeholder="Entrer Le nom complet de "
+                  disabled
+                  value={user.nomComplet}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Date de Début</span>
+                <input
+                  type="Date"
+                  name="dateDebut"
+                  required
+                  onChange={handleAddConge}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Date de la fin</span>
+                <input
+                  type="Date"
+                  name="dateFin"
+                  required
+                  onChange={handleAddConge}
+                />
+              </div>
+
+              <div className="input-box">
+                <span className="details">Solde Congés</span>
+                <input
+                  type="double"
+                  name="soldeConge"
+                  placeholder="Entrer le solde des congés"
+                  disabled
+                  value={user.soldeConge}
+                />
+              </div>
             </div>
-
+            <div className="button">
+              <input type="button" value="Enregistrement" onClick={addConge} />
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AjoutDemandeConges;
--- src/pages/EmployéPage/GestionCongés.js
+++ src/pages/EmployéPage/GestionCongés.js
@@ -1,56 +1,97 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Logo from '../../components/Logo';
-import Navigation from '../../components/Navigation';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../components/Logo";
+import Navigation from "../../components/Navigation";
 
 const GestionCongés = () => {
-    return (
-        <div>
-            <Logo />
-            <Navigation />
-            <h1>Gestion des Congés</h1>
-            <center>
-                <div className="buttonC w-50 ">
-                    <NavLink to="/AjoutDemandeConges" >
-                        <input className="buttonB" type="button" value="Déposer une demande de Congés" />
-                    </NavLink>
-                </div>
-            </center>
-            <table className="content-tableC">
-                <thead>
-                    <tr>
-                        <th>Rang</th>
-                        <th>Nom et Prénom</th>
-                        <th>Matricule</th>
-                        <th>Date début</th>
-                        <th>Date Fin</th>
-                        <th>Période</th>
-                        <th>Décision</th>
-                        <th>Action</th>
+  const [conges, setConges] = useState([]);
+  const [user, setUser] = useState({});
 
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <NavLink to="/ModifierConge" title="Modifier">
-                                <i class="material-icons">&#xE8B8;</i>
-                            </NavLink>
-                            <a href="/SupprimerEmployes" className="delete" title="Delete"><i class="material-icons">&#xE5C9;</i></a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+    getConges();
 
+    return () => {};
+  }, []);
+
+  const getConges = () => {
+    axios
+      .get(
+        `http://localhost:8080/conge/empl/${localStorage.getItem("username")}`,
+        {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+      )
+      .then((r) => {
+        setConges(r.data);
+        console.log("sucess", conges);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>Gestion des Congés</h1>
+      <center>
+        <div className="buttonC w-50 ">
+          <NavLink to="/AjoutDemandeConges">
+            <input
+              className="buttonB"
+              type="button"
+              value="Déposer une demande de Congés"
+            />
+          </NavLink>
         </div>
-    );
+      </center>
+      <table className="content-tableC">
+        <thead>
+          <tr>
+            <th>Rang</th>
+            <th>Nom et Prénom</th>
+            <th>Matricule</th>
+            <th>Date début</th>
+            <th>Date Fin</th>
+            <th>Période</th>
+            <th>Décision</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {conges?.length > 0 && (
+            <>
+              {conges.map((c, index) => {
+                return (
+                  <tr>
+                    <td>{index}</td>
+                    <td>{user.nomComplet}</td>
+                    <td>{user.matricule}</td>
+                    <td>{c.dateDebut}</td>
+                    <td>{c.dateFin}</td>
+                    <td>{c.periode}</td>
+                    <td>
+                      <NavLink to="/ModifierConge" title="Modifier">
+                        <i class="material-icons">&#xE8B8;</i>
+                      </NavLink>
+                      <a
+                        href="/SupprimerEmployes"
+                        className="delete"
+                        title="Delete"
+                      >
+                        <i class="material-icons">&#xE5C9;</i>
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default GestionCongés;
